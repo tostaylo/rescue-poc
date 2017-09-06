d38cdd4696055ff9d8595c5664dba2c222fdde83
Nolin Naidoo (C) <ZZM5SL@NAM.corp.gm.com>
2017-09-06 18:49:07 -0500

    implement firebase add and read

--- src/app/portal/help/Help.js
+++ src/app/portal/help/Help.js
@@ -1,67 +1,44 @@
-import React from 'react';
-import { PropTypes } from 'prop-types';
-import { Field, reduxForm } from 'redux-form';
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 import { FormattedMessage } from 'react-intl';
-import { Button, Footer, TextField } from 'app/components';
+import { Footer } from 'app/components';
+import firebase from 'root/firebase';
 
-const validate = values => {
-  const errors = {};
-  const requiredFields = ['email', 'password'];
-  requiredFields.forEach(field => {
-    if (!values[field]) {
-      errors[field] = <FormattedMessage id={'form.required'} />;
-    }
-  });
-  if (
-    values.email &&
-    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-  ) {
-    errors.email = <FormattedMessage id={'form.invalidEmail'} />;
+/*eslint-disable*/
+class HelpForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { markers: [] }; // <- set up react state
   }
-  return errors;
-};
-
-const helpForm = ({ handleSubmit, pristine, reset, submitting }) => (
-  <div className="help">
-    <div className="portal-content">
-      <h1>
-        <FormattedMessage id={'app.nav.help'} />
-      </h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <Field
-            name="email"
-            component={TextField}
-            label={<FormattedMessage id={'helpForm.email'} />}
-          />
+  addMessage(e) {
+    e.preventDefault(); // <- prevent form submit from reloading the page
+    /* Send the marker to Firebase */
+    firebase
+      .database()
+      .ref('markers')
+      .push(this.inputEl.value);
+    this.inputEl.value = ''; // <- clear the input
+  }
+  render() {
+    return (
+      <div className="help">
+        <div className="portal-content">
+          <h1>
+            <FormattedMessage id={'app.nav.help'} />
+          </h1>
+          <form onSubmit={this.addMessage.bind(this)}>
+            <input type="text" ref={el => (this.inputEl = el)} />
+            <input type="submit" />
+          </form>
         </div>
-        <div>
-          <Button
-            raised
-            disabled={pristine || submitting}
-            label={<FormattedMessage id={'form.submit'} />}
-          />
-          <Button
-            raised
-            disabled={pristine || submitting}
-            label={<FormattedMessage id={'form.reset'} />}
-            onClick={reset}
-          />
-        </div>
-      </form>
-    </div>
-    <Footer />
-  </div>
-);
+        <Footer />
+      </div>
+    );
+  }
+}
 
-helpForm.propTypes = {
-  handleSubmit: PropTypes.func,
-  reset: PropTypes.func,
-  submitting: PropTypes.bool,
-  pristine: PropTypes.bool
-};
-
-export default reduxForm({
-  form: 'LoginForm',
-  validate
-})(helpForm);
+HelpForm = reduxForm({
+  form: 'HelpForm',
+})(HelpForm);
+/*eslint-enable*/
+export default HelpForm;
--- src/app/portal/respond/Respond.js
+++ src/app/portal/respond/Respond.js
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Footer } from 'app/components';
+import firebase from 'root/firebase';
 
-const Respond = () => (
-  <div className="respond">
-    <div className="portal-content">
-      <h1>
-        <FormattedMessage id={'app.nav.respond'} />
-      </h1>
-    </div>
-    <Footer />
-  </div>
-);
+/*eslint-disable*/
+class Respond extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { markers: [] }; // <- set up react state
+  }
+  componentWillMount() {
+    /* Create reference to markers in Firebase Database */
+    const messagesRef = firebase
+      .database()
+      .ref('markers')
+      .orderByKey()
+      .limitToLast(100);
+    messagesRef.on('child_added', snapshot => {
+      /* Update React state when marker is added at Firebase Database */
+      const marker = { text: snapshot.val(), id: snapshot.key };
+      this.setState({ markers: [marker].concat(this.state.markers) });
+    });
+  }
+  render() {
+    return (
+      <div className="respond">
+        <div className="portal-content">
+          <h1>
+            <FormattedMessage id={'app.nav.respond'} />
+          </h1>
+          <ul>
+            {/* Render the list of markers */
+            this.state.markers.map(marker => (
+              <li key={marker.id}>{marker.text}</li>
+            ))}
+          </ul>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
+/*eslint-enable*/
 
 export default Respond;
